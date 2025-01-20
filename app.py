fb0f7d0427c28225f413ce420dbd927cef8666eb
MOHAMMED-SAEED-OMER <muhammadsahed55@gmail.com>
2025-01-20 17:08:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,61 +16,59 @@ if not st.session_state["logged_in"]:
     from login import render_login
     render_login()
 else:
-    # Sidebar Navigation with Pages
-    st.sidebar.title("Navigation")
-    page = st.sidebar.radio(
-        "Go to:",
-        options=[
-            "Database",
-            "Add New Data",
-            "Submit a Request",
-            "Approver",
-            "Payment",
-            "Liquidation",
-            "User Profiles"
-        ]
-    )
+    st.markdown("<h1 style='text-align: center; color: #1E3A8A;'>Finance Management System</h1>", unsafe_allow_html=True)
 
-    if page == "Database":
-        from database import render_database
-        render_database()
+    # Horizontal navigation bar with tabs
+    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
+        "Submit a Request", 
+        "Approver", 
+        "Payment", 
+        "Liquidation", 
+        "Database", 
+        "Add Data", 
+        "User Profiles"
+    ])
 
-    elif page == "Add New Data":
-        if st.session_state["user_role"] in ["Admin", "Requester"]:
-            from add_data import render_add_data
-            render_add_data()
-        else:
-            st.warning("You do not have permission to access this page.")
-
-    elif page == "Submit a Request":
+    with tab1:
         if st.session_state["user_role"] in ["Admin", "Requester"]:
             from submit_request import render_request_form
             render_request_form()
         else:
             st.warning("You do not have permission to access this page.")
 
-    elif page == "Approver":
+    with tab2:
         if st.session_state["user_role"] in ["Admin", "Approver"]:
             from approver_page import render_approver_page
             render_approver_page()
         else:
             st.warning("You do not have permission to access this page.")
 
-    elif page == "Payment":
+    with tab3:
         if st.session_state["user_role"] in ["Admin", "Approver"]:
-            from payment_page import render_payment_page  # Use the correct function name
+            from payment_page import render_payment_page
             render_payment_page()
         else:
             st.warning("You do not have permission to access this page.")
 
-    elif page == "Liquidation":
-        if st.session_state["user_role"] == "Admin":
+    with tab4:
+        if st.session_state["user_role"] in ["Admin"]:
             from liquidation_page import render_liquidation_page
             render_liquidation_page()
         else:
             st.warning("You do not have permission to access this page.")
 
-    elif page == "User Profiles":
+    with tab5:
+        from database import render_database
+        render_database()
+
+    with tab6:
+        if st.session_state["user_role"] in ["Admin", "Requester"]:
+            from add_data import render_add_data
+            render_add_data()
+        else:
+            st.warning("You do not have permission to access this page.")
+
+    with tab7:
         if st.session_state["user_role"] == "Admin":
             from user_profiles import render_user_profiles
             render_user_profiles()
